8321d8318f5aabe48fd70a98673d5164a6e35b94
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2021-10-19 19:37:44 +0545

    Add substring

---
+++ Ruby-path/Ruby-projects/sub_string.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+def substrings(text, dictionary)
+  dictionary.each_with_object(Hash.new(0)) do |value, total|
+    matches = text.scan(Regexp.new(value, Regexp::IGNORECASE))
+    total[value] = matches.length unless matches.length.zero?
+  end
+end
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+pp substrings('below', dictionary)
+pp substrings("Howdy partner, sit down! How's it going?", dictionary)
